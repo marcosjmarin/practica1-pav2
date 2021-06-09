64f900a29840080ff36e5b603cc93900e3145be7
marcosjmarin <marcosjmarin@hotmail.com>
2021-06-09 13:11:28 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,19 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
-  text = 'A ver ahora la concha de tu madre'
+  text = 'Listado Articulos';
+  articulos: any[] = [
+    {
+      id: 2,
+      nombre: 'Articulo X'
+    },
+    {
+      id: 5,
+      nombre: 'Articulo Y'
+    }
+  ];
 }
